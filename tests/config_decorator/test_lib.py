1a1af9faf860d71cdd3c911fbf1eac312561ae46
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-12-23 20:39:36 -0600

    Tests: Expand test coverage.

--- tests/config_decorator/test_lib.py
+++ tests/config_decorator/test_lib.py
@@ -23,17 +23,105 @@ import logging
 import pytest
 
 from config_decorator import section
+from config_decorator.config_decorator import ConfigDecorator
+from config_decorator.subscriptable import Subscriptable
 
 
-class TestConfigDecorator:
-    @pytest.mark.parametrize('storetype', ['sqlalchemy'])
-#    def test_get_store_valid(self, controller, storetype):
-    def test_get_store_valid(self, storetype):
-        """Make sure we recieve a valid ``store`` instance."""
-        return
-        # [TODO]
-        # Once we got backend registration up and running this should be
-        # improved to check actual store type for that backend.
-        controller.config['db.store'] = storetype
-        assert isinstance(controller._get_store(), BaseStore)
+class TestConfigDecoratorEmpty:
+    # 2019-12-23: (lb): First test. 20% coverage.
+    def test_empty_config(self):
+        @section(None)
+        class ConfigRoot(object):
+            pass
+
+        rootcfg = ConfigRoot
+        assert isinstance(rootcfg, ConfigDecorator)
+        assert isinstance(rootcfg._innerobj, object)
+
+
+class TestConfigDecoratorNamed:
+    # 2019-12-23: (lb): Second test. 21% coverage.
+    def test_named_config(self):
+        @section('foo', parent=None)
+        class ConfigRoot(object):
+            pass
+
+        rootcfg = ConfigRoot
+        assert isinstance(rootcfg, ConfigDecorator)
+        assert isinstance(rootcfg._innerobj, object)
+
+
+class TestConfigDecoratorNested:
+    # 2019-12-23: (lb): Second test. 22% coverage.
+
+    def test_nested_config(self):
+        @section(None)
+        class ConfigRoot(object):
+            pass
+
+        @ConfigRoot.section('foo')
+        class NestedConfig(Subscriptable):
+            pass
+
+
+
+class TestConfigDecoratorOverlay:
+    # 2019-12-23: (lb): Third test. 31% coverage.
+
+    def test_section_overlay(self):
+        @section(None)
+        class ConfigRoot(object):
+            def inner_function(self):
+                return 'bar'
+
+        @ConfigRoot.section(None)
+        class ConfigRootOverlay(Subscriptable):
+            def __init__(self):
+                pass
+
+            @property
+            @ConfigRoot.setting(
+                "Generated value.",
+                hidden=True,
+            )
+            def inner_function(self):
+                return ConfigRoot._innerobj.inner_function()
+
+
+class TestConfigDecoratorSameSectionRef:
+    def test_same_section_ref(self):
+        @section(None)
+        class ConfigRoot(object):
+            pass
+
+        @ConfigRoot.section('foo')
+        class ConfigRootFoo1(Subscriptable):
+            def __init__(self):
+                pass
+
+        @ConfigRoot.section('foo')
+        class ConfigRootFoo2(Subscriptable):
+            def __init__(self):
+                pass
+
+
+class TestConfigDecoratorPassive:
+    def test_root_section_passive(self):
+        @section
+        class ConfigRoot(object):
+            pass
+
+
+class TestSectionSettingDocstringDoc:
+    def test_section_setting_implied_doc(self):
+        @section(None)
+        class ConfigRoot(object):
+            pass
+
+        @ConfigRoot.section(None)
+        class ConfigRootReal(object):
+            @property
+            @ConfigRoot.setting()
+            def foo(self):
+                '''The foo setting does bar.'''
 
